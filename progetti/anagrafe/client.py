ac3d5203600e040ba5c711b84b85abb7f9e557b8
EdoardoDiLisio <edoardo.dl@libero.it>
2024-10-11 14:38:34 +0200

    modifiche organizzative

--- progetti/anagrafe/client.py
+++ progetti/anagrafe/client.py
@@ -12,8 +12,13 @@ def RichiediDatiCittadino():
     jRequest = {"nome":nome, "cognome":cognome, "data nascita":dataNascita,"codice fiscale":codFiscale }
     return jRequest
 
+def CercaCittadino():
+    codFiscale = input("Inserisci codice fiscale: ")
+    jRequest = {"codice fiscale": codFiscale}
+    return jRequest
+
 def CreaInterfaccia():
-    print("Operazioni disponibili:")
+    print("Operazioni disponibili: ")
     print("1. Inserisci cittadino (es. atto di nascita)")
     print("2. Richiedi dati cittadino (es. cert. residenza)")
     print("3. Modifica dati cittadino")
@@ -23,16 +28,19 @@ def CreaInterfaccia():
 CreaInterfaccia()
 sOper = input("Seleziona operazione: ")
 while (sOper != "5"):
-    if sOper=="1":
+    if sOper == "1":
         api_url = base_url + "/add_cittadino"
         jsonDataRequest = RichiediDatiCittadino()
-        try:
-            response = requests.post(api_url,json=jsonDataRequest)
-            print(response.status_code)
-            print(response.headers["Content-Type"])
-            data1 = response.json()
-            print(data1)
-        except:
-            print("Problemi di comunicazione con il server, riprova più tardi")
+    elif sOper =="2":
+        api_url = base_url + "/get_cittadino"
+        jsonDataRequest = CercaCittadino()
+    try:
+        response = requests.post(api_url,json=jsonDataRequest)
+        print(response.status_code)
+        print(response.headers["Content-Type"])
+        data1 = response.json()
+        print(data1)
+    except:
+        print("Problemi di comunicazione con il server, riprova piÃ¹ tardi")
     CreaInterfaccia()
-    sOper = input("Seleziona operazione: ")
+    sOper = input("Seleziona operazione")
--- progetti/anagrafe/server.py
+++ progetti/anagrafe/server.py
@@ -1,19 +1,13 @@
 from flask import Flask, json, request
 from myjson import JsonSerialize,JsonDeserialize
-import sys
 
-# sFile = "./prova.json"
-# myDict = {"nome":"Mario", "cognome":"Rossi"}
-# iRet = JsonSerialize(myDict,sFile)
-# if iRet == 0:
-#     print("Operazione terminata correttamente")
-# elif iRet == 1:
-#     print("Errore, dati errati, atteso dizionario!")
-# elif iRet == 2:
-#     print("Errore, salvataggio su file")
-# sys.exit()
 
-sFileAnagrafe = "./anagrafe.json"
+lListaCampil = ["nome", "cognome", "data nascita", "codice fiscale"]
+# if campoRicevutoDalClient in lListaCampi:
+    
+
+
+Anagrafe = "./anagrafe.json"
 api = Flask(__name__)
 
 @api.route('/add_cittadino', methods=['POST'])
@@ -26,19 +20,39 @@ def GestisciAddCittadino():
         sCodiceFiscale = jRequest["codice fiscale"]
         print("Ricevuto " + sCodiceFiscale)
         #carichiamo l'anagrafe
-        dAnagrafe = JsonDeserialize(sFileAnagrafe)
+        dAnagrafe = JsonDeserialize(Anagrafe)
         if sCodiceFiscale not in dAnagrafe:
             dAnagrafe[sCodiceFiscale] = jRequest
-            JsonSerialize(dAnagrafe,sFileAnagrafe)
-            jResponse = {"Error":"000", "Msg": "ok"}
+            JsonSerialize(dAnagrafe,Anagrafe)
+            jResponse = {"Error":"000", "Msg": "Cittadino inserito"}
             return json.dumps(jResponse),200
         else:
             jResponse = {"Error":"001", "Msg": "codice fiscale gia presente in anagrafe"}
             return json.dumps(jResponse),200
     else:
         return "Errore, formato non riconosciuto",401
-    #controlla che il cittadino non è gia presente in anagrafe
+    #controlla che il cittadino non Ã¨ gia presente in anagrafe
     #rispondi
+
+@api.route('/get_cittadino', methods = ['POST'])
+def GestisciGetCittadino():
+    # gestisco la richiesta
+    content_type = request.headers.get('Content-Type')
+    print("Ricevuta chiamata" + content_type)
+    if content_type == "application/json":
+        jRequest = request.json
+        sCodiceFiscale = jRequest["codice fiscale"]
+        print("Ricevuto" + sCodiceFiscale)
+    # carico l'anagrafe
+        dAnagrafe = JsonDeserialize(Anagrafe)
+        if sCodiceFiscale in dAnagrafe:
+            dJsonRespons = dAnagrafe[sCodiceFiscale]
+            return json.dumps(dJsonRespons),200
+        else:
+            jResponse = {"Error":"001", "Msg": "codice fiscale gia presente in anagrafe"}
+            return json.dumps(jResponse),200
+
+
 
 
 api.run(host="127.0.0.1", port=8080)
